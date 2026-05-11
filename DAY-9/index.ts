bacd50f2fb9110b536972b62cc44f3b89b5aad29
vamsivikkurty <vamsivikkurty12345@gmail.com>
2026-05-11 12:47:10 +0530

    typescript

--- DAY-9/index.ts
+++ DAY-9/index.ts
@@ -1,2 +1,14 @@
-let name = "Vamsi";
-console.log(name);
+//let name = "Vamsi";
+//console.log(name);
+
+let name: string = "Aaryan";
+let result: number | string;
+result = 45;
+result = "success";
+console.log(name, result);
+ 
+function add(a: number, b: number): string {
+    return "Sum: " + (a + b);
+}
+ 
+console.log(add(5, 10));
