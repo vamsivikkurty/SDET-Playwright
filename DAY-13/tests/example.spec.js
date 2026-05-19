cee4e0526cf7375b309b4f90491ba44712d247d6
vamsivikkurty <vamsivikkurty12345@gmail.com>
2026-05-19 14:02:01 +0530

    updated

--- DAY-13/tests/example.spec.js
+++ DAY-13/tests/example.spec.js
@@ -1,19 +1,38 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+const loginData = [
+  {
+    username: "tom", 
+    password: "tom", 
+    expected: "Username length must be greater than 3 & Password 5"
+  }, {
+    username: "tom123", 
+    password: "tom", 
+    expected: "Username length must be greater than 3 & Password 5"
+  }, {
+    username: "admin123", 
+    password: "admin123", 
+    expected: "Login successful"
+  }, {
+    username: "admin123", 
+    password: "admin12345", 
+    expected: "Invalid credentials"
+  },
+]
+loginData.forEach(async data => {
+  test(`Checking Login form with ${data.username}, ${data.password}`, async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+  
+    const username = page.locator("#username");
+    const password = page.locator("#password");
+    const btn = page.locator("#loginBtn");
+    const msg = page.locator("#msg");
+  
+    await username.fill(data.username);
+    await password.fill(data.password);
+    await btn.click();
+    
+    await expect(msg).toHaveText(data.expected);
+  });
+})
--- DAY-13/webapp/index.js
+++ DAY-13/webapp/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', async (req, res) => {
